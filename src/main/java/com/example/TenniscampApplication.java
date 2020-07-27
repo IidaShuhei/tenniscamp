ad21d1b253ae80bd25e806c0d8df23cd9ddda726
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-07-27 18:57:42 +0900

    first

---
+++ src/main/java/com/example/TenniscampApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenniscampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenniscampApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/domain/Player.java
@@ -0,0 +1,11 @@
+package com.example.domain;
+
+import lombok.Data;
+
+@Data
+public class Player {
+
+	private Integer playerId;
+	private String playerName;
+	
+}
